9b76d255df1e45a31680fa85c568d5e43cbd9fc8
ChengTao <chengtaolearn@163.com>
2016-09-16 01:14:51 +0800

    完成分页功能

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,34 @@ var result = require('../entity/resultEntity');
 var assert = require('assert');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var pages = 8;
+    var index = req.query.index == undefined ? 1 : req.query.index;
+    console.log(index);
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://localhost/cuihu');
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
         var query = cuiHuModel.find({});
+        query.sort({updateAt : 'desc'}).skip((index-1)*pages).limit(pages+1);
         var promise = query.exec();
         promise.then(function (data) {
             result.MultiData = data;
-            result.currentCount = data.length;
+            var currentCount = 0;
+            var isNext = false;
+            var currentIndex = index;
+            if(data.length > pages){
+                currentCount = data.length - 1;
+                isNext = true;
+            }else {
+                currentCount = data.length;
+            }
             res.render('index',{
                 title : '翠湖观察信息浏览',
-                data : result
+                data : result,
+                currentCount : currentCount,
+                isNext : isNext,
+                currentIndex : currentIndex
             });
             mongoose.disconnect();
         });
